1850265dc3681b50619ef6b090acd146f0e767dc
matheusgomes062 <matheusgomes062@gmail.com>
2020-09-07 21:56:38 -0300

    routes, file read, http and other basic stuff

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-});
+const routes = require("./routes");
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
